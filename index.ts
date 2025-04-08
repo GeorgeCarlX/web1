6789f5d30e917757023e8035bb64c83cefe8088e
GeorgeCarlX <m15502697000@outlook.com>
2025-04-08 22:08:11 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,9 +3,11 @@ export default {
     async fetch(request: Request) {
         const url = new URL(request.url);
         if (url.pathname === '/api') {
-            return new Response("Hello from Cloudflare Worker!");
+            const response = await fetch('https://web1.ddragonwfish.workers.dev/index.html'); // 替换为你的实际域名
+            return response;
+            //return new Response("Hello from Cloudflare Worker!");
         } else if (url.pathname === '/' || url.pathname === '/index.html') {
-            const response = await fetch('https://web1.ddragonwfish.workers.dev/'); // 替换为你的实际域名
+            const response = await fetch('https://web1.ddragonwfish.workers.dev/index.html'); // 替换为你的实际域名
             return response;
         }
         return new Response("Not Found", { status: 404 });
