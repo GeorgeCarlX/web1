5e159422d9169124bbdd23605b2b89502cd29ca8
GeorgeCarlX <m15502697000@outlook.com>
2025-04-08 22:04:10 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
+// index.ts
 export default {
     async fetch(request: Request) {
         const url = new URL(request.url);
         if (url.pathname === '/api') {
             return new Response("Hello from Cloudflare Worker!");
+        } else if (url.pathname === '/' || url.pathname === '/index.html') {
+            const response = await fetch('https://web1.ddragonwfish.workers.dev/'); // 替换为你的实际域名
+            return response;
         }
         return new Response("Not Found", { status: 404 });
     },
